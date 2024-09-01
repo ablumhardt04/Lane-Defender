5475f6ce78a772ae24a4364046da9790dfc465f2
ablumhardt04 <ablumhardt@mail.bradley.edu>
2024-08-31 22:37:46 -0500

    Snails done, enemy spawner in progress
    Going to create the other enemies to insert them into the spawner

--- Assets/Scripts/Foe.cs
+++ Assets/Scripts/Foe.cs
@@ -7,12 +7,15 @@ public class Foe : MonoBehaviour
     private SpriteRenderer sr;
     private Animator animator;
     private Bullet moveScript;
+    [SerializeField] private int _health;
     [SerializeField] private Sprite _walk1;
     [SerializeField] private Sprite _walk2;
     [SerializeField] private Sprite _hit;
     [SerializeField] private Sprite _dead;
     [SerializeField] private int _spriteState;
     private bool hit;
+    private bool dead;
+    private Color initialColor;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,7 @@ public class Foe : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         moveScript = GetComponent<Bullet>();
+        initialColor = sr.color;
     }
 
     public void ChangeSprite(int state)
@@ -47,15 +51,49 @@ public class Foe : MonoBehaviour
 
     private void Update()
     {
+        // Knows it's been hit when it detects it has entered damage sprite
         if (!hit && (_spriteState == 2))
         {
             hit = true;
             moveScript.PauseMove(true);
+            sr.color = Color.white;
         }
-        else if (hit && (_spriteState != 2))
+        else if (hit && !dead && (_spriteState != 2))
         {
             hit = false;
             moveScript.PauseMove(false);
+            sr.color = initialColor;
+        }
+
+        if (!dead && (_spriteState == 3))
+        {
+            dead = true;
+            Death();
+        }
+
+        if ((Mathf.Abs(transform.position.x) > 20) || (Mathf.Abs(transform.position.y) > 20))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Sets a variety of effects to indicate death
+    /// </summary>
+    private void Death()
+    {
+        moveScript.enabled = false;
+        Rigidbody2D rb2D = GetComponent<Rigidbody2D>();
+        rb2D.bodyType = RigidbodyType2D.Dynamic;
+        rb2D.gravityScale = 1;
+        rb2D.constraints = RigidbodyConstraints2D.None;
+        rb2D.freezeRotation = false;
+        rb2D.velocity = new Vector2(20, 10);
+        rb2D.angularVelocity = 360;
+        GetComponent<Collider2D>().enabled = false;
+        if ((transform.childCount > 0) && (transform.GetChild(0).name == "Shadow"))
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 
@@ -63,7 +101,15 @@ public class Foe : MonoBehaviour
     {
         if (collision.gameObject.tag == "Bullet")
         {
-            animator.SetTrigger("hit");
+            if (_health > 1)
+            {
+                animator.SetTrigger("hit");
+                _health--;
+            }
+            else
+            {
+                animator.SetBool("dead", true);
+            }
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,8 +10,11 @@ public class GameManager : MonoBehaviour
     private PlayerInput playerInputInstance;
     private InputAction restart;
 
+    private float _enemyStartTime;
     [SerializeField] private float _enemyIntensity;
     [SerializeField] private GameObject _snailPrefab;
+    private int[] lastLanesChosen = { -1, -1, -1 };
+    private float[] _snailPositions = { 4f, 2.1f, 0.15f, -1.8f, -3.75f };
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +24,7 @@ public class GameManager : MonoBehaviour
         restart = playerInputInstance.currentActionMap.FindAction("Restart");
         restart.started += Restart_started;
 
+        _enemyStartTime = Time.time;
         StartCoroutine(EnemySpawner());
     }
 
@@ -37,11 +41,94 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator EnemySpawner()
     {
+        // initial wave
+        _enemyIntensity = 0;
+        SpawnEnemy(1);
+        yield return new WaitForSeconds(0.5f);
+        SpawnEnemy(5);
+
         while (true)
         {
-            Instantiate(_snailPrefab);
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(LengthBetweenSpawns());
+            _enemyIntensity = (Time.time - _enemyStartTime) % 5;
+            SpawnEnemy(SelectRandomEnemy());
         }
+    }
+
+    private float LengthBetweenSpawns()
+    {
+        float length = 3f;
+        if (_enemyIntensity >= 2) // 10 seconds have passed
+        {
+            length = 2.5f;
+        }
+        if (_enemyIntensity >= 4) // 20 seconds have passed
+        {
+            length = 2f;
+        }
+        if (_enemyIntensity >= 12) // 60 seconds have passed
+        {
+            length = 0.5f;
+        }
+        length += UnityEngine.Random.Range(0, 1f) - 0.5f;
+        return Mathf.Max(0.01f, length);
+    }
+
+    private void SpawnEnemy(int health)
+    {
+        // Chooses a random lane different from the last three lanes chosen
+        int lane;
+        bool goodLane = false;
+        do
+        {
+            goodLane = true;
+            lane = UnityEngine.Random.Range(0, 5);
+            for (int i = 0; i < 3; i++)
+            {
+                if (lane == lastLanesChosen[i])
+                {
+                    goodLane = true;
+                }
+            }
+        }
+        while (goodLane == false);
+        lastLanesChosen[2] = lastLanesChosen[1];
+        lastLanesChosen[1] = lastLanesChosen[0];
+        lastLanesChosen[0] = lane;
+
+        // Spawns enemy
+        if (health == 5)
+        {
+            Instantiate(_snailPrefab, new Vector2(10, _snailPositions[lane]), Quaternion.identity, transform);
+        }
+        else if (health == 3)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private int SelectRandomEnemy()
+    {
+        // Chooses enemy's health at random, which determines which enemy it will be
+        if (_enemyIntensity <= 4) // 20 seconds have passed
+        {
+            int[] options = { 5, 3, 3, 1, 1, 1 };
+            return options[UnityEngine.Random.Range(0, options.Length)];
+        }
+        if (_enemyIntensity <= 8) // 40 seconds have passed
+        {
+            int[] options = { 5, 3, 1 };
+            return options[UnityEngine.Random.Range(0, options.Length)];
+        }
+        if (_enemyIntensity <= 12) // 60 seconds have passed
+        {
+            return 5;
+        }
+        return 1;
     }
 
     private void OnDestroy()
